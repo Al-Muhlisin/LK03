c3ced992cbdcc735ccd07c4901198626c2e2871d
Al-Muhlisin <muhlisinaal@gmail.com>
2026-03-13 13:28:29 +0700

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,40 @@
+public abstract class Product {
+    private String productId;
+    private String name;
+    private double price;
+    private int stockQuantity;
+
+    public Product() {}
+
+    public Product(String productId, String name, double price, int stockQuantity) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public abstract double calculateDiscount();
+    
+    public void updateStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    public void updateStock(int quantity, String reason) {
+        this.stockQuantity += quantity;
+        System.out.println("Stok diperbarui karena " + reason);
+    }
+    
+    public String getName() {
+         return name; 
+    }
+    public double getPrice() { 
+        return price; 
+    }
+    public int getStockQuantity() { 
+        return stockQuantity; 
+    }
+    
+    public void getProductInfo() {
+        System.out.printf("ID: %s | Nama: %s | Harga: %.2f | Stok: %d%n", 
+                          productId, name, price, stockQuantity);
+    }
+}
