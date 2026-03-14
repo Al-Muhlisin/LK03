eaad2926a62befc746d7f55497e59709f13090ae
Tomenjeri <118422913+Sadaharujr15@users.noreply.github.com>
2026-03-14 04:28:49 +0000

    Add ElectronicProduct and update FoodProduct

---
+++ ElectronicProduct.java
@@ -0,0 +1,27 @@
+public class ElectronicProduct extends Product {
+    private String warrantyPeriod;
+
+    public ElectronicProduct() {}
+
+    public ElectronicProduct(String productId, String name, double price, int stockQuantity, String warrantyPeriod) {
+        super(productId, name, price, stockQuantity);
+        this.warrantyPeriod = warrantyPeriod;
+    }
+
+    @Override
+    public double calculateDiscount() {
+        double discountPercent = 0.05;
+        if (getPrice() > 500000) {
+            discountPercent += 0.02; 
+        }
+        return getPrice() * discountPercent;
+    }
+
+    public String getWarrantyPeriod() {
+        return warrantyPeriod;
+    }
+
+    public void setWarrantyPeriod(String warrantyPeriod) {
+        this.warrantyPeriod = warrantyPeriod;
+    }
+}
